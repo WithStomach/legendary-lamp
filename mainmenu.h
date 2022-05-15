e8d6f7d99d2fff8a5df7fc59cf271be094fdcd64
WithStomach <2100013106@stu.pku.edu.cn>
2022-05-15 21:27:07 +0800

    new change

---
+++ mainmenu.h
@@ -0,0 +1,17 @@
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+#include <QWidget>
+
+class MainMenu : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit MainMenu(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // MAINMENU_H
